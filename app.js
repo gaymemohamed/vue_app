28a3d388e01b7826cb288a858160373d482bfa7d
gaymemohamed <gaymemohamed@gmail.com>
2022-05-17 23:21:36 +0200

    click event

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const app = Vue.createApp({
+    // data & functions
+    // template: '<h2>Iam Template </h2>',
     data(){
         return {
             title: "gamal",
             gender: "male",
             age: "25"
         }
+    },
+    methods:{
+        changeTitle(title){
+            // this.title = "Abdo"
+            this.title = title;
+        }
     }
 })
 
+// to run this vue code you should mount the code to the element that we will run in it;
 app.mount('#app')
